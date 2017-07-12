d9808f9b9f79248b3451c72ee14f6dec50f72841
Alex Miller <asmiller1989@gmail.com>
2017-07-12 16:11:16 -0700

    Adding solidity-coverage tool

--- truffle.js
+++ truffle.js
@@ -1,9 +1,17 @@
 module.exports = {
   networks: {
     development: {
+      gas: 0xfffffff,
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    coverage: {
+      host: "localhost",
+      network_id: "*",
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01 
     }
   }
 };
